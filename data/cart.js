0bb9847d0d18871829ef52698e80974787d3cee8
IgorPanasik <panasik.igor.com@gmail.com>
2023-09-03 22:51:26 +0300

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,33 @@
 // Export variable for access in other files
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem; // Variable for save corresponding producta with unique Id
+
+  // Product matching check
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  //Add select quantity products
+  const selectQuantity = document.querySelector(
+    `.js-quantity-selector-${productId}` // Use `` template strings!!!!
+  );
+
+  const quantity = Number(selectQuantity.value); // Covert value from string to number(default string)
+
+  // if the product is already in the cart calculete quantity else add product in cart
+  if (matchingItem) {
+    matchingItem.quantity += quantity; //insead += 1 , add anything from select
+  } else {
+    cart.push({
+      //productId: productId,
+      //quantity: quantity,
+      //Shortcat destructuring
+      productId,
+      quantity,
+    });
+  }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
-// Import variable from cart.js
-import { cart } from "../data/cart.js";
+// Import variables from cart.js and products.js
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 //We want to take all of our objects to have the same structure so that our code can handle each product the same.  from 'products.js'
@@ -76,102 +76,77 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML; // concate
 
 // Find to add to cart all buttons through DOM than we can loop through each of the buttons.
 
+function updateCartQuantity(productId) {
+  // Calculate total quantity products in cart
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  // We're going to use an object to save the timeout ids.
+  // The reason we use an object is because each product
+  // will have its own timeoutId. So an object lets us
+  // save multiple timeout ids for different products.
+  //const addedMessageTimeouts = {};
+
+  // This solution uses a feature of JavaScript called a
+  // closure. Each time we run the loop, it will create
+  // a new variable called addedMessageTimeoutId and do
+  // button.addEventListener().
+  //
+  // Then, because of closure, the function we give to
+  // button.addEventListener() will get a unique copy
+  // of the addedMessageTimeoutId variable and it will
+  // keep this copy of the variable forever.
+  // (Reminder: closure = if a function has access to a
+  // value/variable, it will always have access to that
+  // value/variable).
+  //
+  // This allows us to create many unique copies of the
+  // addedMessageTimeoutId variable (one for every time
+  // we run the loop) so it lets us keep track of many
+  // timeoutIds (one for each product).
+  let addedMessageTimeoutId;
+
+  //Add message 'ADDED' first find to selector
+  const addedMessage = document.querySelector(
+    `.js-added-message-${productId}` // Use `` template strings!!!!
+  );
+
+  addedMessage.classList.add("added-to-cart-message");
+  // Use setTimeout for removing the class
+
+  setTimeout(() => {
+    // Check if there's a previous timeout for this
+    // product. If there is, we should stop it.
+    //const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if (addedMessageTimeoutId) {
+      //previousTimeoutId) {
+      clearTimeout(addedMessageTimeoutId); //previousTimeoutId);
+    }
+
+    const setTimeoutID = setTimeout(() => {
+      addedMessage.classList.remove("added-to-cart-message");
+    }, 2000);
+
+    // Save the timeoutId for this product
+    // so we can stop it later if we need to.
+    //addedMessageTimeouts[productId] = setTimeoutID;
+    addedMessageTimeoutId = setTimeoutID;
+  });
+
+  // Add carQuantity on the page
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     //const productId = button.dataset.productId; // Save to product Id in variable
     //Shortcat destructuring
     const { productId } = button.dataset;
-    let matchingItem; // Variable for save corresponding producta with unique Id
-
-    // Product matching check
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    //Add select quantity products
-    const selectQuantity = document.querySelector(
-      `.js-quantity-selector-${productId}` // Use `` template strings!!!!
-    );
-
-    const quantity = Number(selectQuantity.value); // Covert value from string to number(default string)
-
-    // if the product is already in the cart calculete quantity else add product in cart
-    if (matchingItem) {
-      matchingItem.quantity += quantity; //insead += 1 , add anything from select
-    } else {
-      cart.push({
-        //productId: productId,
-        //quantity: quantity,
-        //Shortcat destructuring
-        productId,
-        quantity,
-      });
-    }
-
-    // Calculate total quantity products in cart
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    // We're going to use an object to save the timeout ids.
-    // The reason we use an object is because each product
-    // will have its own timeoutId. So an object lets us
-    // save multiple timeout ids for different products.
-    //const addedMessageTimeouts = {};
-
-    // This solution uses a feature of JavaScript called a
-    // closure. Each time we run the loop, it will create
-    // a new variable called addedMessageTimeoutId and do
-    // button.addEventListener().
-    //
-    // Then, because of closure, the function we give to
-    // button.addEventListener() will get a unique copy
-    // of the addedMessageTimeoutId variable and it will
-    // keep this copy of the variable forever.
-    // (Reminder: closure = if a function has access to a
-    // value/variable, it will always have access to that
-    // value/variable).
-    //
-    // This allows us to create many unique copies of the
-    // addedMessageTimeoutId variable (one for every time
-    // we run the loop) so it lets us keep track of many
-    // timeoutIds (one for each product).
-    let addedMessageTimeoutId;
-
-    //Add message 'ADDED' first find to selector
-    const addedMessage = document.querySelector(
-      `.js-added-message-${productId}` // Use `` template strings!!!!
-    );
-
-    addedMessage.classList.add("added-to-cart-message");
-    // Use setTimeout for removing the class
-
-    setTimeout(() => {
-      // Check if there's a previous timeout for this
-      // product. If there is, we should stop it.
-      //const previousTimeoutId = addedMessageTimeouts[productId];
-
-      if (addedMessageTimeoutId) {
-        //previousTimeoutId) {
-        clearTimeout(addedMessageTimeoutId); //previousTimeoutId);
-      }
-
-      const setTimeoutID = setTimeout(() => {
-        addedMessage.classList.remove("added-to-cart-message");
-      }, 2000);
-
-      // Save the timeoutId for this product
-      // so we can stop it later if we need to.
-      //addedMessageTimeouts[productId] = setTimeoutID;
-      addedMessageTimeoutId = setTimeoutID;
-    });
-
-    // Add carQuantity on the page
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity(productId);
   });
 });
