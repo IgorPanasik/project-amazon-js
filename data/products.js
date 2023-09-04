ae61081af3740986c59433d1cc09e4552478ed1f
IgorPanasik <panasik.igor.com@gmail.com>
2023-09-04 14:59:12 +0300

    Generate HTML for the order summary

--- data/products.js
+++ data/products.js
@@ -3,7 +3,6 @@
 export const products = [
   {
     //First product in object
-
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg", // Image first prodoct
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs", // Name first product
